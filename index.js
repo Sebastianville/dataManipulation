a5a64dc41e02d00233e0b6876546e7eec7d51496
Sebastianville <sebastian.villegas2623@gmail.com>
2025-01-08 13:40:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+let firstName = 'Alex'
+
+let msg = "Hello, we are reaching out to you about your extended warranty"
+
+const age = 35;
+
+let isMsg = false; 
+
+let alertMsg = `${msg}. \n ${firstName} we are aware you are ${age} years old`
+console.log(alertMsg)
+
+
+
+let addition = (5 + 10) * 2 
+
+console.log(addition)
+
+console.log(5 === 10)
+console.log(5 === '10')
+console.log(5 !== 10)
+
+
+console.log(5 == 20)
+console.log(5 == "20")
+console.log(5 != 20)
+
+
